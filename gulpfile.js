1d8054dac65b25d2bdb8563759aad0659daad77e
Adam Richardson <adsric@icloud.com>
2015-02-15 23:37:13 +0000

    Update frontend toolkit

--- gulpfile.js
+++ gulpfile.js
@@ -10,28 +10,55 @@ var runSequence = require('run-sequence');
 var browserSync = require('browser-sync');
 var reload      = browserSync.reload;
 
-var AUTOPREFIXER_BROWSERS = [
-    'Android >= 4.4',
-    'Chrome >= 34',
-    'Firefox >= 24', // Firefox 24 is the latest ESR
-    'Explorer >= 9',
-    'iOS >= 7',
-    'Opera >= 12.1',
-    'Safari >= 6'
-];
+// Lint JavaScript
+gulp.task('jshint', function () {
+    return gulp.src(['js/**/*.js', '!js/vendor/*.js', '!js/plugins/*.js',  '!js/**/*.min.js'])
+        .pipe(reload({stream: true, once: true}))
+        .pipe($.jshint())
+        .pipe($.jshint.reporter('jshint-stylish'))
+        .pipe($.if(!browserSync.active, $.jshint.reporter('fail')))
+});
 
-var messages = {
-    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
-};
+// JavaScript
+gulp.task('scripts', function () {
+    return gulp.src(['js/**/*.js', '!js/vendor/*.js', '!js/main.min.js'])
+        // Concatenate And Minify Scripts
+        .pipe($.concat('main.js'))
+        .pipe($.rename({suffix: '.min'}))
+        .pipe($.uglify())
+        .pipe(gulp.dest('_site/js'))
+        .pipe(gulp.dest('js'))
+        .pipe($.size({title: 'scripts'}));
+});
 
-// Compile Sass and prefix Styles
-gulp.task('sass', function () {
+// Optimize Images
+gulp.task('images', function () {
+    return gulp.src('img/**/*')
+        .pipe($.imagemin({
+            progressive: true,
+            interlaced: true
+        }))
+        .pipe(gulp.dest('_site/img'))
+        .pipe($.size({title: 'images'}));
+});
+
+// Compile and prefix Stylesheets.
+gulp.task('styles', function () {
+
+    var AUTOPREFIXER_BROWSERS = [
+        '> 1%',
+        'last 2 versions',
+        'ie 9',
+        'Firefox ESR',
+        'Opera 12.1'
+    ];
+
     return gulp.src('css/*.scss')
         .pipe($.changed('sass', {extension: '.scss'}))
         .pipe($.sass({
-            precision: 10
+            precision: 10,
+            onError: console.error.bind(console, 'Sass error:')
         }))
-        .on('error', console.error.bind(console))
         .pipe($.autoprefixer({browsers: AUTOPREFIXER_BROWSERS}))
         .pipe(gulp.dest('_site/css'))
         .pipe(gulp.dest('css'))
@@ -40,38 +67,16 @@ gulp.task('sass', function () {
         .pipe($.rename({suffix: '.min'}))
         .pipe(gulp.dest('_site/css'))
         .pipe(gulp.dest('css'))
-        .pipe($.size({title: 'css'}));
-});
-
-// JavaScript
-gulp.task('js', function () {
-    return gulp.src(['js/**/*.js', '!js/vendor/*.js', '!js/**/*.min.js'])
-        .pipe(reload({stream: true, once: true}))
-        .pipe($.jshint())
-        .pipe($.jshint.reporter('jshint-stylish'))
-        .pipe($.if(!browserSync.active, $.jshint.reporter('fail')))
-        // Concatenate And Minify Scripts
-        .pipe($.concat('app.js'))
-        .pipe($.rename({suffix: '.min'}))
-        .pipe($.uglify())
-        .pipe(gulp.dest('_site/js'))
-        .pipe(gulp.dest('js'))
-        .pipe($.size({title: 'js'}));
-});
-
-// Optimize Images
-gulp.task('imgs', function () {
-    return gulp.src('img/**/*')
-        .pipe($.imagemin({
-            progressive: true,
-            interlaced: true
-        }))
-        .pipe(gulp.dest('_site/img'))
-        .pipe($.size({title: 'imgs'}));
+        .pipe($.size({title: 'styles'}));
 });
 
 // Jekyll Build.
 gulp.task('jekyll', function (done) {
+
+    var messages = {
+        jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+    };
+
     browserSync.notify(messages.jekyllBuild);
     return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
         .on('close', done);
@@ -83,7 +88,7 @@ gulp.task('jekyll-rebuild', ['jekyll'], function () {
 });
 
 // Jekyll Server
-gulp.task('serve', ['jekyll', 'sass', 'js'], function() {
+gulp.task('serve', ['jshint', 'jekyll', 'styles', 'scripts'], function() {
     browserSync({
         notify: false,
         browser: 'google chrome canary',
@@ -93,9 +98,10 @@ gulp.task('serve', ['jekyll', 'sass', 'js'], function() {
     // Watch Files For Changes & Reload
     gulp.watch(['_includes/*.html', '_layouts/*.html'], ['jekyll-rebuild']);
     gulp.watch(['*.{html,md}', '_posts/*'], ['jekyll-rebuild']);
-    gulp.watch('css/**/*.scss', ['sass', reload]);
-    gulp.watch('js/**/*.js', ['js', reload]);
-    gulp.watch('img/**/*', ['imgs', reload]);
+    gulp.watch('css/**/*.scss', ['styles', reload]);
+    gulp.watch('js/**/*.js', ['jshint', reload]);
+    gulp.watch('js/**/*.js', ['scripts', reload]);
+    gulp.watch('img/**/*', ['images', reload]);
 });
 
 // Clean Output Directory
@@ -105,5 +111,5 @@ gulp.task('clean', function (done) {
 
 // Build Files
 gulp.task('default', function (done) {
-    runSequence('clean', ['jekyll', 'sass', 'js', 'imgs'], done);
+    runSequence('clean', ['jshint', 'jekyll', 'styles', 'scripts', 'images'], done);
 });
